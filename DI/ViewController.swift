75a58f8a5b716806e791b27ccca2173f5f651377
Rin <rin.t.4921@gmail.com>
2022-02-03 23:38:12 +0900

    initial commit

---
+++ DI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI
+//
+//  Created by Rin on 2022/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
